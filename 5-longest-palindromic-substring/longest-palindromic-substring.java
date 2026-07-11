3381915dcec2f5129e7013c0613ade39ff8621d9
devsh56 <100186354+devsh56@users.noreply.github.com>
2026-07-11 17:54:47 +0530

    Time: 264 ms (18.90%) | Memory: 48.6 MB (5.53%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,31 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
-        Boolean dp[][]=new Boolean[s.length()][s.length()];
-       /* for(int [] arr:dp){
-            Arrays.fill(arr,-1);
-        }*/
-        int maxlen = 1;
-        int index = 0;
+        boolean dp[][]=new boolean[s.length()][s.length()];
+        int index=0;
+        int maxlen=1;
         for(int i =0 ;i<s.length();i++){
-            for(int j = i+1;j<s.length();j++){
-                if(Palindrome(i,j,s,dp)){
-                   if (j - i + 1 > maxlen) {
-        maxlen = j - i + 1;
-        index = i;
-    }
+            dp[i][i] = true;
+        }
+
+        for(int L=2 ;L<=s.length();L++){
+            for(int i =0 ;i<s.length();i++){
+                int j = i+L-1;
+                if(j <= s.length()-1 && s.charAt(i) == s.charAt(j) && L == 2) {
+                    dp[i][j] = true;
+                    if(maxlen < L){
+                        index = i;
+                        maxlen = L;
+                    }
+                }
+                else if(j <= s.length()-1 && s.charAt(i)  == s.charAt(j) && dp[i+1][j-1]){
+                    dp[i][j] = true;
+                    if(maxlen < L){
+                        index = i;
+                        maxlen = L;
+                    }
                 }
             }
         }
         return s.substring(index,index+maxlen);
-    }
-    public static boolean Palindrome(int i,int j , String s,Boolean dp[][]){
-            if(i>=j) return true;
-
-            if(dp[i][j] != null) return dp[i][j];
-            boolean flag = false;
-            if(s.charAt(i) == s.charAt(j)){
-                flag = flag || Palindrome(i+1,j-1,s,dp);
-            }
-            return dp[i][j] = flag;
     }
 }
