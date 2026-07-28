e85dfc9a2b774c016316aa4a38311d630821cc66
devsh56 <100186354+devsh56@users.noreply.github.com>
2026-07-29 00:52:29 +0530

    Time: 63 ms (6.89%) | Memory: 131.9 MB (30.02%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,69 +1,73 @@
-public class Node {
-    Node next;
-    Node prev;
-    int Key;
-    int Value;
-    public Node(int key,int value){
-        this.Key=key;
-        this.Value = value;
-    }
-}
-
 class LRUCache {
-    public HashMap<Integer,Node> map;
-    public Node head;
-    public Node tail;
-    public int size =0;
-    public int Count =0;
+    private int count = 0;
+    private int LruSize = 0;
+    class Node{
+        int v;
+        int k;
+        Node next;
+        Node prev;
+        public Node(int val,int key){
+            this.v = val;
+            this.k = key;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    private Node head;
+    private Node tail;
+    private Map<Integer,Node> map;
     public LRUCache(int capacity) {
-       size = capacity;
-       head = new Node(-1,-1);
-       tail = new Node(-1,-1);
-       map = new HashMap<Integer,Node>(); 
-       head.next = tail;
-       tail.prev = head;
+            head = new Node(0,-1);
+            tail = new Node(0,-1);
+            head.next = tail;
+            tail.prev = head;
+            map=new HashMap<>();
+            LruSize = capacity;
     }
     
     public int get(int key) {
-         if(!map.containsKey(key))
-        return -1;
-    Node node = map.get(key);
-    removeNode(node);
-    addNode(node);
-    return node.Value;
+        if(map.get(key) == null) return -1;
+        Node latest = map.get(key);
+        map.remove(latest.k);
+        removeNode(latest);
+        Node gg = addNode(latest.v,latest.k);
+        map.put(latest.k,gg);
+        return latest.v;
     }
     
     public void put(int key, int value) {
         if(map.containsKey(key)){
-        Node node = map.get(key);
-        removeNode(node);
-        map.remove(key);
-        Count--;
-    }
-        if(size == Count){
-            Node a = removeNode(tail.prev);
-           map.remove(a.Key);
-           Count--;
+            Node repeated = map.get(key);
+            removeNode(repeated);
+            map.remove(key);
+            count--;
         }
-        Node aa = new Node(key,value);
-        map.put(key,aa);
-        addNode(aa);
-        Count++;
-        
+        if(count == LruSize){
+            Node leastUsed = head.next;
+         //   System.out.println(leastUsed.k);
+            map.remove(leastUsed.k);
+            removeNode(leastUsed);
+            count--;
+        }
+        Node added = addNode(value,key);
+        map.put(key,added);
+        count++;
     }
-    public void addNode(Node dd){
-        Node temp = head.next;
-        head.next = dd;
-        temp.prev = dd;
-        dd.next = temp;
-        dd.prev = head;
+
+    public Node addNode(int v,int key){
+        Node aa = new Node(v,key);
+        Node g = tail.prev;
+        g.next = aa;
+        aa.prev=g;
+        aa.next=tail;
+        tail.prev=aa;
+        return aa;
     }
-    public Node removeNode(Node dd){
-        Node Next = dd.next;
-        Node Prev = dd.prev;
-        Prev.next = Next;
-        Next.prev = Prev;
-        return dd;
+    public void removeNode(Node a){
+        Node p = a.prev;
+        Node n = a.next;
+        p.next=n;
+        n.prev=p;
     }
 }
 
